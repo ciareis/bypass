9c7f1987dc2f70367a4556ba27e5dd556ea71455
Leandro Henrique <emtudo@gmail.com>
2021-05-21 16:55:42 -0300

    remove space

--- config/bypass.php
+++ config/bypass.php
@@ -9,6 +9,5 @@ return [
     | Indicate below where your PHP is installed. For instance:
     | On Windows systems: 'c:\php\php.exe'. On unix systems: '/usr/bin/php'
     */
-
     'php_path' => 'php'
 ];
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,7 +9,6 @@ class TestCase extends \Orchestra\Testbench\TestCase
 
     protected function getPackageProviders($app)
     {
-
         return [
             BypassServiceProvider::class
         ];
