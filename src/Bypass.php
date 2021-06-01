9bf7e51b43703b775eee14d498323f5a6990881e
Leandro Henrique <emtudo@gmail.com>
2021-06-01 20:15:56 -0300

    added type to times

--- src/Bypass.php
+++ src/Bypass.php
@@ -118,7 +118,7 @@ class Bypass
     }
 
     // @todo deprecated: It will remove at version v1.0.0
-    public function expect(string $method, string $uri, int $status = 200, ?string $body = null, $times = 1)
+    public function expect(string $method, string $uri, int $status = 200, ?string $body = null, int $times = 1)
     {
         return $this->addRoute($method, $uri, $status, $body, $times);
     }
