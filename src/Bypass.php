0fcc6fea2716f4db2099ad7449cab7e08e81a939
Mateus Guimarães <mateus.jatene@gmail.com>
2021-06-12 15:18:51 -0300

    accept body as an array

--- src/Bypass.php
+++ src/Bypass.php
@@ -82,8 +82,10 @@ class Bypass
         return $this;
     }
 
-    public function addRoute(string $method, string $uri, int $status = 200, ?string $body = null, int $times = 1): self
+    public function addRoute(string $method, string $uri, int $status = 200, string|array $body = null, int $times = 1): self
     {
+        $body = is_array($body) ? json_encode($body) : $body;
+
         $this->addRouteParams($uri, [
             'method' => \strtoupper($method),
             'content' => $body,
@@ -128,7 +130,7 @@ class Bypass
     }
 
     // @todo deprecated: It will remove at version v1.0.0
-    public function expect(string $method, string $uri, int $status = 200, ?string $body = null, int $times = 1): self
+    public function expect(string $method, string $uri, int $status = 200, string|array $body = null, int $times = 1): self
     {
         return $this->addRoute($method, $uri, $status, $body, $times);
     }
